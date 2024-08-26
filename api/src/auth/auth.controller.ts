00779ef9d39fe94d9274db43edb06be5271807e6
Robert <robert.garcia222@yahoo.com>
2024-08-26 11:47:51 -0500

    added sanitize, validate, and transform to signUp DTO

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,10 +1,23 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Transform } from 'class-transformer';
+import * as sanitizeHtml from 'sanitize-html';
 
-type SignUpDto = {
+export class SignUpDto {
+    @IsNotEmpty()
+    @Transform((params) => sanitizeHtml(params.value))
     name: string;
+
+    @IsEmail()
+    @Transform((params) => sanitizeHtml(params.value))
     email: string;
+
+    @IsNotEmpty()
+    @Transform((params) => sanitizeHtml(params.value))
     username: string;
+
+    @IsNotEmpty()
     password: string;
 }
 
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
     signUp(signUpDto) {
-        console.log("Sign Up Dto: ", signUpDto);
+      return signUpDto;
     } 
 }
