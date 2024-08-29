a3646f1fb472791c1705c7cd024730229ddc14da
Robert <robert.garcia222@yahoo.com>
2024-08-28 20:03:33 -0500

    Header:Initial Nav setup

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,9 +1,33 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Image, Heading } from '@chakra-ui/react';
+import logo from './logo';
+import { Link } from 'react-router-dom';
+
+const pages = [
+  {name: "Log In", path: "/log-in"},
+  {name: "Create Account", path: "/sign-up"},
+  {name: "Projects", path: "/projects"},
+  {name: "Account Details", path: "/profile"},
+]
 
 const Header = () => {
+
+
   return (
-    <Box>
-      Header
+    <Box p={4} display="flex" border="1px solid" alignItems="center">
+      <Box display="flex" gap={4}  alignItems="center">
+        <Image 
+          src={logo} 
+          alt='logo' 
+          boxSize="70px" 
+          objectFit="cover"
+        />
+        <Heading fontSize={24}>Project Planning Tool</Heading>
+      </Box>
+      <Box display="flex" justifyContent="space-around" w="70%">
+        {pages.map((page) => {
+          return (<Link to={page.path}><Box>{page.name}</Box></Link>);
+        })}
+      </Box>
     </Box>
   );
 }
